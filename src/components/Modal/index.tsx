7502c7ecf7ea6ca0b730febfb348da70fd4685f8
George W F Santos <gweverton21@hotmail.com>
2021-05-15 22:35:46 -0300

    finish challenge

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,54 @@
+import React, { ReactNode, useEffect, useState } from "react";
+import ReactModal from "react-modal";
+
+interface ModalProps {
+  children: ReactNode;
+  setIsOpen: () => void;
+  onRequestClose?: (
+    event: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>
+  ) => void;
+  isOpen: boolean;
+}
+
+const Modal = (props: ModalProps) => {
+  const { children, setIsOpen, isOpen } = props;
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(() => {
+    if (modalStatus !== isOpen) {
+      console.log(props);
+      setModalStatus(isOpen);
+    }
+  }, [isOpen, modalStatus]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#F0F0F5",
+          color: "#000000",
+          borderRadius: "8px",
+          width: "736px",
+          border: "none",
+        },
+        overlay: {
+          backgroundColor: "#121214e6",
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
